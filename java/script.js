9db10cd88ebb939adb30002bde82347eb69cd0c7
robertkutwa <robertkutwa0@gmail.com>
2025-02-03 22:37:38 +0300

    adding homepage features and form

--- java/script.js
+++ java/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+$(document).ready(function () {
+    $("#bookingForm").submit(function (event) {
+        event.preventDefault();
+        var bookingDate = $("#datepicker").val();
+        alert("Booking date: " + bookingDate);
+    });
+});
+function myFunction() {
+    var x = document.getElementsByClassName("navbar")[0];
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+$(function () {
+    $("#datepicker").datepicker({ minDate: 0 }); // minDate: 0 makes past dates disabled
+});
